39bcf4c1408c6d8088512d064ac8e43a2d33b1d8
vivekkumar <vivek24ee066@satiengg.in>
2024-09-14 18:31:01 +0530

    txt commit

--- registration/urls.py
+++ registration/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls'))
+    path('',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('home/',views.HomePage,name='home'),
+    path('logout/',views.LogoutPage,name='logout'),
+
 ]
